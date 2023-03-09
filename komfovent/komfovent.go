ee6fd9d9c8e0f709ca3751e19ee7a92a5fdd330f
Jaanek Oja <jaanekoja@gmail.com>
2023-03-09 11:39:34 +0200

    refactoring

---
+++ komfovent/komfovent.go
@@ -0,0 +1,19 @@
+package komfovent
+
+import "github.com/jaanek/hassext/homeassistant"
+
+type OperationMode string
+
+const (
+	OperationModeAway      OperationMode = "1"
+	OperationModeNormal    OperationMode = "2"
+	OperationModeIntensive OperationMode = "3"
+	OperationModeBoost     OperationMode = "4"
+)
+
+const operationModeAddress uint = 4
+const operationModeUnit byte = 1
+
+func SetOperationMode(modbus homeassistant.Modbus, mode OperationMode) error {
+	return modbus.ModbusWriteRegister("Komfovent", operationModeAddress, operationModeUnit, string(mode))
+}
